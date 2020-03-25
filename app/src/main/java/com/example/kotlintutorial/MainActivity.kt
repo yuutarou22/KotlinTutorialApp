5529f1c804e352383db2963573d74ae55646a853
YutaroM <yutaroapp@gmail.com>
2020-03-26 07:20:42 +0900

    ・コメント追加（Kotlinすごい）

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -16,20 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // XMLに定義しているコンポーネントを直で参照できる！すげー！
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
 
+    // 画面右上に配置されるオプションメニューを作成する
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
+    // 画面右上のオプションメニューの要素を選択した時に呼ばれる
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        //
+        // whenはswitch文のようなもの。Javaとは異なり、String比較出来る。
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
